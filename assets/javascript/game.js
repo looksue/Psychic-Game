5e05ae4c3e6564013ee6c7bbdaed82c096bc2c23
Sue Look <suelook1@gmail.com>
2019-07-26 15:36:21 -0400

    screen writes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -12,12 +12,27 @@ end
 // declare variables
 var intWins = 0;
 var intLosses = 0;
-var intGuessesLeft = 9; 
+var intGuessesLeft = 0; 
 var lettersPicked = "";
+var currentGuess = "";
 var winLetter = "";
 var alphArray = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 
-// pick a letter
+beginGame:
+// pick a letter and set guesses
 winLetter = alphArray[Math.floor(Math.random() * 26)];
+intGuessesLeft = 9;
 
-console.log (winLetter);
+// begin looping
+while (currentGuess != winLetter) {
+    document.clear();
+    document.write("Guess what letter I'm thinking of: _");
+    document.write("");
+    document.write("Wins: " + intWins);
+    document.write("");
+    document.write("Losses: " + intLosses);
+    document.write("");
+    document.write("Guesses Left: " + intGuessesLeft);
+    document.write("");
+    document.write("Guesses so far: " + lettersPicked);
+}
