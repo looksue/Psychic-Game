f17d9ee1e4353355bd1dbea0c7c5ce135e96649f
Sue Look <suelook1@gmail.com>
2019-07-26 14:50:00 -0400

    declared variables

---
+++ assets/javascript/game.js
@@ -0,0 +1,20 @@
+/*
+start program
+declare variables
+pick a letter
+while false
+write the screen
+accept guesses/updates
+loop
+end 
+*/
+
+// declare variables
+var intWins = 0;
+var intLosses = 0;
+var intGuessesLeft = 9; 
+var lettersPicked = "";
+var winLetter = "";
+
+
+Math.floor(Math.random() * 26);
