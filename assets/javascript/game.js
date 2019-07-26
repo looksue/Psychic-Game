04690c81e037e613e95e63147d5530f3781577a7
Sue Look <suelook1@gmail.com>
2019-07-26 15:05:16 -0400

    random letter generator

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -15,6 +15,9 @@ var intLosses = 0;
 var intGuessesLeft = 9; 
 var lettersPicked = "";
 var winLetter = "";
+var alphArray = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 
+// pick a letter
+winLetter = alphArray[Math.floor(Math.random() * 26)];
 
-Math.floor(Math.random() * 26);
+console.log (winLetter);
